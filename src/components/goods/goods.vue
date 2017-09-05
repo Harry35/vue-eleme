24c4fdec69980c27c274b6de6cb781a63b08468e
Xu <charleszx35@gmail.com>
2017-09-05 23:16:13 +0200

    header

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    I am goods
+  </div>
+</template>
+
+<script type="text/exmascript-6">
+  export default {
+
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
