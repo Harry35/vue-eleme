348bd54da6b3919eb04de5417907817b7137d3f4
Xu <charleszx35@gmail.com>
2017-09-12 23:43:13 +0200

    component star

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -32,6 +32,10 @@
     <div v-show="detailShow" class="detail">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <div class="star-wrapper">
+            <star :size="48" :score="seller.score"></star>
+          </div>
         </div>
       </div>
       <div class="detail-close">
@@ -42,6 +46,8 @@
 </template>
 
 <script type="text/exmascript-6">
+  import star from '../../components/star/star';
+
   export default {
     props: {
       seller: {
@@ -60,6 +66,9 @@
     },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    components: {
+      star
     }
   };
 </script>
@@ -188,9 +197,19 @@
       background: rgba(7,17,27,0.8)
       .detail-wrapper
         min-height: 100%
+        width: 100%
         .detail-main
           margin-top: 64px
           padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+          .star-wrapper
+            margin-top: 18px
+            padding: 2px 0px
+            text-align: center
       .detail-close
         position: relative
         width: 32px
