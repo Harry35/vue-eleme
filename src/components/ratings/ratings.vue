c568dd9cb4fbb55cfedf0ec36d2e683b9ce11310
Xu <charleszx35@gmail.com>
2017-10-05 23:00:19 +0200

    continue ratings

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="ratings">
     <div class="ratings-content">
-
+      <div class="overview">
+        <div class="overview-left"></div>
+        <div class="overview-right"></div>
+      </div>
     </div>
   </div>
 </template>
