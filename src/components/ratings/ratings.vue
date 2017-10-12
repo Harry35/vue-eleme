4c80d0e48e373aeb933b5442db1b4b2ea3342178
Xu <charleszx35@gmail.com>
2017-10-12 23:49:39 +0200

    ratings

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -36,7 +36,24 @@
     padding: 18px
     .overview-left
       flex: 0 0 137px
+      padding: 6px 0
       width: 137px
+      border-right: 1px solid rgba(7,17,27,0.1)
+      text-align: center
+      .score
+        margin-bottom: 6px
+        line-height: 28px
+        font-size: 24px
+        color: rgb(255, 153, 0)
+      .title
+        margin-bottom: 8px
+        line-height: 12px
+        font-size: 12px
+        color: rgb(7, 17, 27)
+      .rank
+        line-height: 10px
+        font-size: 10px
+        color: rgb(147, 153, 159)
     .overview-right
       flex: 1
       padding-left: 24px
